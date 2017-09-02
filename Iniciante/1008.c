548669583867518a4bdbb4fd863fee0305e5b021
gessefelipe <gessefelipe.gf@hotmail.com>
2017-09-02 15:29:23 -0300

    first commit

---
+++ Iniciante/1008.c
@@ -0,0 +1,26 @@
+/*
+
+URI Online Judge | 1008
+
+Salário
+
+Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+Entrada
+
+O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
+Saída
+
+Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+
+*/
+
+#include <stdio.h>
+
+int main(void) 
+{
+    int func, horastrabalhadas;
+    float salariohora;
+    scanf("%d %d %f", &func, &horastrabalhadas, &salariohora);
+    printf("NUMBER = %d\nSALARY = U$ %0.2f\n", func, horastrabalhadas*salariohora);
+    return 0;
+}
